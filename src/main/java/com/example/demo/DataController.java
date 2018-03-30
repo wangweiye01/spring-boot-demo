303428bb881c7cb4ccdeda46e395aaa10b23c54e
wangweiye <wwyknight@163.com>
2018-03-30 10:14:28 +0800

    fix

--- src/main/java/com/example/demo/DataController.java
+++ src/main/java/com/example/demo/DataController.java
@@ -6,17 +6,21 @@ import com.example.demo.entity.Manager;
 import com.example.demo.repository.CompanyRepository;
 import com.example.demo.repository.DepartmentRepository;
 import com.example.demo.repository.ManagerRepository;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@Api(value = "测试controller", tags = {"测试JPA接口"})
 public class DataController {
     @Autowired
     private ManagerRepository managerRepository;
@@ -30,17 +34,19 @@ public class DataController {
     @ApiOperation(value = "保存部门信息", notes = "")
     @ApiImplicitParams({
             @ApiImplicitParam(paramType = "header", name = "Authorization", value = "token", required = true, dataType = "string"),
+            @ApiImplicitParam(name = "managerName", value = "经理姓名", dataType = "int", required = true, paramType = "form"),
+            @ApiImplicitParam(name = "companyName", value = "公司名称", dataType = "string", required = true, paramType = "form")
     })
     @RequestMapping(value = "/department/ready", method = RequestMethod.POST)
-    public void departmentReady() {
+    public ResponseEntity<?> departmentReady(@RequestParam String managerName, @RequestParam String companyName) {
         Manager manager = new Manager();
-        manager.setName("manger w");
+        manager.setName(managerName);
 
         Department department = new Department();
         department.setName("department y");
 
         Company company = new Company();
-        company.setName("立德");
+        company.setName(companyName);
 
 
         manager.setDepartment(department);
@@ -49,14 +55,20 @@ public class DataController {
         company.getDepartmentSet().add(department);
 
 
-        companyRepository.save(company);
+        return ResponseEntity.ok(companyRepository.save(company));
     }
 
-    @RequestMapping("/departments")
-    public void departments() {
+    @ApiOperation(value = "查询所有部门", notes = "")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "header", name = "Authorization", value = "token", required = true, dataType = "string"),
+    })
+    @RequestMapping(value = "/departments", method = RequestMethod.GET)
+    public ResponseEntity<?> departments() {
         List<Department> departments = departmentRepository.findAll();
 
         departments.forEach(x -> System.out.println(x.getName()));
+
+        return ResponseEntity.ok(departments);
     }
 
     @RequestMapping("/saveCompany")
